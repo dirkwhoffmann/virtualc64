43c4b1d316af776378ad7ac276bc06eef7ae29b0
dirk.hoffmann <dirk.hoffmann@5fb870d6-7847-0410-b3f5-9f7b564574ea>
2015-01-02 14:43:22 +0000

    ROM images are now drop targets

--- trunk/MacGUI/ROMDropTargetView.mm
+++ trunk/MacGUI/ROMDropTargetView.mm
@@ -21,41 +21,39 @@
 @implementation ROMDropTargetView
 
 @synthesize allowDrop;
-@synthesize delegate;
-
-// NSString *kPrivateDragUTI = @"www.dirkwhoffmann.de.virtualc64";
 
 - (id)initWithCoder:(NSCoder *)coder
 {
-    NSLog(@"ROMDropTargetView:initWithCoder");
-    
     self = [super initWithCoder:coder];
 
     if (self) {
-        // Register pasteboard types
+        
         [self registerForDraggedTypes:@[NSFilenamesPboardType]];
         self.allowDrop = YES;
+        
     }
-    
     return self;
 }
 
-
-
-
 - (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender
 {
-    NSLog(@"ROMDropTargetView:draggingEntered");
-
     if (!self.allowDrop)
         return NSDragOperationNone;
+
+    originalImage = [self image];
 
     NSPasteboard *pb = [sender draggingPasteboard];
     NSString *besttype = [pb availableTypeFromArray:@[NSFilenamesPboardType]];
     
     if (besttype == NSFilenamesPboardType) {
-        NSLog(@"ROMDropTargetView:Dragged in filename");
-        return NSDragOperationCopy;
+
+        NSString *path = [[pb propertyListForType:@"NSFilenamesPboardType"] objectAtIndex:0];
+
+        if ([[controller c64] isRom:path]) {
+            [self setImage:[NSImage imageNamed:@"rom"]];
+            [self setNeedsDisplay];
+            return NSDragOperationCopy;
+        }
     }
     
     return NSDragOperationNone;
@@ -63,44 +61,25 @@
 
 - (void)draggingExited:(id <NSDraggingInfo>)sender
 {
-    NSLog(@"ROMDropTargetView:draggingExited");
-    
-    highlight = NO;
-    [self setNeedsDisplay:YES];
+    if (!self.allowDrop)
+        return;
+
+    [self setImage:originalImage];
+    [self setNeedsDisplay];
 }
 
 - (BOOL)prepareForDragOperation:(id <NSDraggingInfo>)sender
 {
-    NSPasteboard *pb = [sender draggingPasteboard];
-
-    NSLog(@"ROMDropTargetView:prepareForDragOperation");
-    
-    highlight = NO;
-    [self setNeedsDisplay: YES];
-
-    // Did we receive a filename?
-    if (![[pb types] containsObject:NSFilenamesPboardType])
-        return NO;
-    
-    // Did we receive the name of a valid ROM image?
-    NSString *path = [[pb propertyListForType:@"NSFilenamesPboardType"] objectAtIndex:0];
-    NSLog(@"Got filename %@", path);
-
-#if 0
-    // Is it a ROM file? DON'T LOAD HERE!
-    if ([[controller document] loadRom:path]) {
-        NSLog(@"ROM loaded");
-        return YES;
-    }
-#endif
-    
-    return NO;
+    return YES;
 }
 
 - (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
 {
-    NSLog(@"ROMDropTargetView:performDragOperation");
-    return YES;
+    NSPasteboard *pb = [sender draggingPasteboard];
+    
+    // Load ROM
+    NSString *path = [[pb propertyListForType:@"NSFilenamesPboardType"] objectAtIndex:0];
+    return [[controller document] loadRom:path];
 }
 
 
