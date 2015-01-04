3d59639f1b6a0937235fc2e449f4e6a3e9f1f82b
dirk.hoffmann <dirk.hoffmann@5fb870d6-7847-0410-b3f5-9f7b564574ea>
2015-01-04 10:31:36 +0000

    TOD clocks are initialized with the current time

--- C64/C64.h
+++ C64/C64.h
@@ -17,8 +17,8 @@
  */
 
 // New:
-// 1. Fixed a severe bug in the background drawing routine (Bug #149)
-// 2. Cartridge images in the ROM dialog are now drop targets (Bug #132)
+// 1. Time of day clocks are initialized with the current time
+// 2.
 
 #ifndef _C64_INC
 #define _C64_INC
