be6c17ab48ee8135c594eb6a6ccfd63e28e2c8fa
Dirk Hoffmann <hoff@dark-vader.local>
2018-08-29 14:13:06 +0200

    Added Joystick Prefs (not functional)

---
+++ OSX/MacGUI/JoystickPrefsController.swift
@@ -0,0 +1,207 @@
+//
+//  JoystickPrefsController.swift
+//  VirtualC64
+//
+//  Created by Dirk Hoffmann on 29.08.18.
+//
+
+import Foundation
+
+class JoystickPrefsWindow : NSWindow {
+    
+    func respondToEvents() {
+        DispatchQueue.main.async {
+            self.makeFirstResponder(self)
+        }
+    }
+    
+    override func awakeFromNib() {
+        
+        respondToEvents()
+    }
+    
+    override func keyDown(with event: NSEvent) {
+        
+        let controller = delegate as! JoystickPrefsController
+        controller.keyDown(with: MacKey.init(with: event))
+    }
+    
+    override func flagsChanged(with event: NSEvent) {
+        
+        let controller = delegate as! JoystickPrefsController
+        if event.modifierFlags.contains(.shift) {
+            controller.keyDown(with: MacKey.shift)
+        } else if event.modifierFlags.contains(.control) {
+            controller.keyDown(with: MacKey.control)
+        } else if event.modifierFlags.contains(.option) {
+            controller.keyDown(with: MacKey.option)
+        }
+    }
+}
+
+class JoystickPrefsController : UserDialogController {
+    
+    /// Indicates if a keycode should be recorded for keyset 1
+    var recordKey1: JoystickDirection?
+    
+    /// Indicates if a keycode should be recorded for keyset 1
+    var recordKey2: JoystickDirection?
+    
+    /// Joystick directions
+    @IBOutlet weak var left1: NSTextField!
+    @IBOutlet weak var left1button: NSButton!
+    @IBOutlet weak var right1: NSTextField!
+    @IBOutlet weak var right1button: NSButton!
+    @IBOutlet weak var up1: NSTextField!
+    @IBOutlet weak var up1button: NSButton!
+    @IBOutlet weak var down1: NSTextField!
+    @IBOutlet weak var down1button: NSButton!
+    @IBOutlet weak var fire1: NSTextField!
+    @IBOutlet weak var fire1button: NSButton!
+    @IBOutlet weak var left2: NSTextField!
+    @IBOutlet weak var left2button: NSButton!
+    @IBOutlet weak var right2: NSTextField!
+    @IBOutlet weak var right2button: NSButton!
+    @IBOutlet weak var up2: NSTextField!
+    @IBOutlet weak var up2button: NSButton!
+    @IBOutlet weak var down2: NSTextField!
+    @IBOutlet weak var down2button: NSButton!
+    @IBOutlet weak var fire2: NSTextField!
+    @IBOutlet weak var fire2button: NSButton!
+    @IBOutlet weak var disconnectKeys: NSButton!
+    
+    override func awakeFromNib() {
+    
+    }
+    
+    func update() {
+        
+        // Joystick emulation keys
+        updateKeyMap(0, direction: JOYSTICK_UP, button: up1button, txt: up1)
+        updateKeyMap(0, direction: JOYSTICK_DOWN, button: down1button, txt: down1)
+        updateKeyMap(0, direction: JOYSTICK_LEFT, button: left1button, txt: left1)
+        updateKeyMap(0, direction: JOYSTICK_RIGHT, button: right1button, txt: right1)
+        updateKeyMap(0, direction: JOYSTICK_FIRE, button: fire1button, txt: fire1)
+        updateKeyMap(1, direction: JOYSTICK_UP, button: up2button, txt: up2)
+        updateKeyMap(1, direction: JOYSTICK_DOWN, button: down2button, txt: down2)
+        updateKeyMap(1, direction: JOYSTICK_LEFT, button: left2button, txt: left2)
+        updateKeyMap(1, direction: JOYSTICK_RIGHT, button: right2button, txt: right2)
+        updateKeyMap(1, direction: JOYSTICK_FIRE, button: fire2button, txt: fire2)
+        disconnectKeys.state = parent.keyboardcontroller.disconnectEmulationKeys ? .on : .off
+    }
+    
+    func updateKeyMap(_ nr: Int, direction: JoystickDirection, button: NSButton, txt: NSTextField) {
+        
+        precondition(nr == 0 || nr == 1)
+        
+        let keyMap = parent.gamePadManager.gamePads[nr]?.keyMap //  ?? [:]
+        
+        // Which MacKey is assigned to this joystick action?
+        var macKey: MacKey?
+        var macKeyCode: String = ""
+        var macKeyDesc: String = ""
+        for (key, dir) in keyMap! {
+            if dir == direction.rawValue {
+                macKey = key
+                macKeyCode = NSString(format: "%02X", macKey!.keyCode) as String
+                macKeyDesc = macKey?.description?.uppercased() ?? ""
+                break
+            }
+        }
+        
+        // Update text and button image
+        let recordKey = (nr == 0) ? recordKey1 : recordKey2
+        if (recordKey == direction) {
+            button.title = ""
+            button.image = NSImage(named: NSImage.Name(rawValue: "key_red"))
+            button.imageScaling = .scaleAxesIndependently
+        } else {
+            button.image = NSImage(named: NSImage.Name(rawValue: "key"))
+            button.imageScaling = .scaleAxesIndependently
+        }
+        button.title = macKeyCode
+        txt.stringValue = macKeyDesc
+    }
+    
+    //
+    // Keyboard events
+    //
+    
+    func keyDown(with macKey: MacKey) {
+        
+        track()
+        
+        // Check for ESC key
+        if macKey == MacKey.escape {
+            cancelAction(self)
+            return
+        }
+        
+        if recordKey1 != nil {
+            
+            parent.gamePadManager.gamePads[0]?.assign(key: macKey, direction: recordKey1!)
+            recordKey1 = nil
+        }
+        if recordKey2 != nil {
+            
+            parent.gamePadManager.gamePads[1]?.assign(key: macKey, direction: recordKey2!)
+            recordKey2 = nil
+        }
+        
+        update()
+    }
+    
+    //
+    // Action methods
+    //
+    
+    @IBAction func recordKeyAction(_ sender: NSButton!) {
+        
+        let tag = UInt32(sender.tag)
+        
+        if tag >= 0 && tag <= 4 {
+            recordKey1 = JoystickDirection(rawValue: tag)
+            recordKey2 = nil
+        } else if tag >= 10 && tag <= 14 {
+            recordKey1 = nil
+            recordKey2 = JoystickDirection(rawValue: (tag - 10))
+        } else {
+            assert(false);
+        }
+        
+        update()
+    }
+    
+    @IBAction func disconnectKeysAction(_ sender: NSButton!) {
+        
+        parent.keyboardcontroller.disconnectEmulationKeys = (sender.state == .on)
+        update()
+    }
+    
+    @IBAction func helpAction(_ sender: Any!) {
+        
+        if let url = URL.init(string: "http://www.dirkwhoffmann.de/virtualc64/ROMs.html") {
+            NSWorkspace.shared.open(url)
+        }
+    }
+    
+    @IBAction override func cancelAction(_ sender: Any!) {
+        
+        parent.loadEmulatorUserDefaults()
+        hideSheet()
+    }
+    
+    @IBAction func factorySettingsAction(_ sender: Any!) {
+        
+        parent.gamePadManager.restoreFactorySettings()
+        parent.keyboardcontroller.disconnectEmulationKeys = true
+        update()
+    }
+    
+    @IBAction func okAction(_ sender: Any!) {
+        
+        parent.saveEmulatorUserDefaults()
+        hideSheet()
+    }
+}
+
