5f673ca0ef43f4ce5c1de37bc1d0071da21c3a2f
Dirk Hoffmann <dirk.hoffmann@hs-karlsruhe.de>
2018-11-05 19:57:16 +0100

    cleaned up some comments

--- OSX/MacGUI/ImageUtilities.swift
+++ OSX/MacGUI/ImageUtilities.swift
@@ -1,11 +1,12 @@
 //
-// This source file is part of VirtualC64 - A Commodore 64 emulator
+// This file is part of VirtualC64 - A user-friendly Commodore 64 emulator
 //
 // Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
 // Licensed under the GNU General Public License v3
 //
 // See https://www.gnu.org for license information
 //
+
 
 import Foundation
 
