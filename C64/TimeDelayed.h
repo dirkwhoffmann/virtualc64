007a5a6d0aff7e8a84965f14a5487c54e6549f0e
Dirk Hoffmann <dirk.hoffmann@hs-karlsruhe.de>
2018-11-05 12:05:06 +0100

    Cleanup comments

--- C64/TimeDelayed.h
+++ C64/TimeDelayed.h
@@ -1,6 +1,7 @@
 /*!
+ * @header      TimeDelayed.h
  * @author      Dirk W. Hoffmann, www.dirkwhoffmann.de
- * @copyright   Dirk W. Hoffmann, 2018. All rights reserved.
+ * @copyright   Dirk W. Hoffmann, all rights reserved.
  */
 /*              This program is free software; you can redistribute it and/or modify
  *              it under the terms of the GNU General Public License as published by
