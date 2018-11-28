f48ded9c08ee2907b920e3f83610abb45717f642
Dirk Hoffmann <dirk.hoffmann@hs-karlsruhe.de>
2018-11-29 00:01:17 +0100

    Added remote commands setModel, reset, typeText, takeScreenshot

--- OSX/MacGUI/AppleScript.swift
+++ OSX/MacGUI/AppleScript.swift
@@ -9,12 +9,138 @@
 
 import Foundation
 
+// The document of the currently active emulator instance
+private var currentDocument: MyDocument {
+    get {
+        return NSApplication.shared.orderedDocuments.first as! MyDocument
+    }
+}
+        
+// The controller of the currently active emulator instance
+private var currentController: MyController {
+    get {
+        return currentDocument.windowControllers.first as! MyController
+    }
+}
+
+// The emulator proxy of the currently active emulator instance
+private var currentProxy: C64Proxy {
+    get {
+        return currentDocument.c64
+    }
+}
+
+
+//
+// Remote control commands
+//
+
+class VC64ResetCommand: NSScriptCommand {
+    
+    override func performDefaultImplementation() -> Any? {
+        
+        track()
+
+        currentProxy.powerUp()
+        return true
+    }
+}
+
+class VC64SetModelCommand: NSScriptCommand {
+    
+    override func performDefaultImplementation() -> Any? {
+        
+        track()
+
+        if let arguments = evaluatedArguments {
+            if let model = arguments["VC64Model"] as? String {
+                switch model {
+                    
+                case "C64_PAL":
+                    currentProxy.setModel(Int(C64_PAL.rawValue))
+                    
+                case "C64_II_PAL":
+                    currentProxy.setModel(Int(C64_II_PAL.rawValue))
+                    
+                case "C64_OLD_PAL":
+                    currentProxy.setModel(Int(C64_II_PAL.rawValue))
+                    
+                case "C64_NTSC":
+                    currentProxy.setModel(Int(C64_II_PAL.rawValue))
+                    
+                case "C64_II_NTSC":
+                    currentProxy.setModel(Int(C64_II_PAL.rawValue))
+                    
+                case "C64_OLD_NTSC":
+                    currentProxy.setModel(Int(C64_II_PAL.rawValue))
+                    
+                default:
+                    return false
+                }
+                
+                track("Remote control: Setting model to \(model)")
+                return true
+            }
+        }
+        return false
+    }
+}
+
+class VC64TypeTextCommand: NSScriptCommand {
+    
+    override func performDefaultImplementation() -> Any? {
+        
+        track()
+
+        if let arguments = evaluatedArguments {
+            if let text = arguments["VC64Text"] as? String {
+                currentController.keyboardcontroller.type(text)
+                return true
+            }
+        }
+        return false
+    }
+}
+
+class VC64TakeScreenshotCommand: NSScriptCommand {
+    
+    override func performDefaultImplementation() -> Any? {
+        
+        track()
+        
+        if let arguments = evaluatedArguments {
+            track()
+            if let path = arguments["VCPath"] as? String {
+                track()
+                let url = URL(fileURLWithPath: path)
+                let image = currentController.metalScreen.screenshot()
+                let data = image?.tiffRepresentation
+                do {
+                    try data?.write(to: url, options: .atomic)
+                    return true
+                } catch {
+                    track("Remote control: Cannot quicksave screenshot")
+                }
+            }
+        }
+        track()
+        return false
+    }
+}
+
+
+
+// DELETE:
+
 class NotiphyWithParams: NSScriptCommand, NSUserNotificationCenterDelegate {
     
     var notiphy = NSUserNotification()
     var notiphyCtr = NSUserNotificationCenter.default
     
     override func performDefaultImplementation() -> Any? {
+        
+        track()
+        
         let parms = self.evaluatedArguments
         var aTitle = ""
         var aSubTitle = ""
@@ -42,14 +168,5 @@ class NotiphyWithParams: NSScriptCommand, NSUserNotificationCenterDelegate {
         
         
         return true
-    }
-}
-
-extension AppDelegate {
-    
-    func setModelCommand() {
-        
-        
-        track()
     }
 }
