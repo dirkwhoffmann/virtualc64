c9002d13af9f9f270c6918b613d65eafb0ae99e4
Dirk Hoffmann <dirk.hoffmann@hs-karlsruhe.de>
2018-12-07 19:59:45 +0100

    Trying to fix Travis CI problem

--- OSX/VirtualC64Tests/VirtualC64Tests.swift
+++ OSX/VirtualC64Tests/VirtualC64Tests.swift
@@ -20,13 +20,17 @@ class VirtualC64Tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        XCTAssert(false)
     }
 
+    /*
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
+     */
 
 }
