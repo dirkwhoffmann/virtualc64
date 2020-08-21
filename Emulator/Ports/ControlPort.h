2f7db6d8ad99aa1e723463778c09b72fb85dd0fb
Dirk Hoffmann <dirk.hoffmann@me.com>
2020-08-21 10:12:37 +0200

    Adding asserts to force-crash unimplemented _save()

--- Emulator/Ports/ControlPort.h
+++ Emulator/Ports/ControlPort.h
@@ -104,7 +104,7 @@ private:
     
     size_t _size() override { COMPUTE_SNAPSHOT_SIZE }
     size_t _load(u8 *buffer) override { LOAD_SNAPSHOT_ITEMS }
-    size_t _save(u8 *buffer) override { SAVE_SNAPSHOT_ITEMS }
+    size_t _save(u8 *buffer) override { assert(false); SAVE_SNAPSHOT_ITEMS }
     
     void oldDidLoadFromBuffer(u8 **buffer) override;
 
--- Emulator/VICII/VICII.h
+++ Emulator/VICII/VICII.h
@@ -713,7 +713,7 @@ private:
     
     size_t _size() override { COMPUTE_SNAPSHOT_SIZE }
     size_t _load(u8 *buffer) override { LOAD_SNAPSHOT_ITEMS }
-    size_t _save(u8 *buffer) override { SAVE_SNAPSHOT_ITEMS }
+    size_t _save(u8 *buffer) override { assert(false); SAVE_SNAPSHOT_ITEMS }
     
     
     //
