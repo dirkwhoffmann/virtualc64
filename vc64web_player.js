ad419ef53d64ec2dbac88bb0c796f06c5deeaf59
mithrendal <daniel.giebener@web.de>
2021-01-14 22:04:28 +0100

    default button visibility and open in new tab same size as other icons

--- vc64web_player.js
+++ vc64web_player.js
@@ -10,24 +10,27 @@ function load_title(element, params, address) {
 
     //turn picture into iframe
     var emuview_html = `
+<div style="display:flex;flex-direction:column;height:100%">
 <iframe id="vc64web" width="100%" height="100%"
     src="https://dirkwhoffmann.github.io/virtualc64web/${params}#${address}">
 </iframe>
-<div style="display: flex;margin-top:-5px"><svg  class="icon_btn" onclick="stop_emu_view();" xmlns="http://www.w3.org/2000/svg" width="2.0em" height="2.0em" fill="currentColor" class="bi bi-pause-btn" viewBox="0 0 16 16">
+<div style="display: flex"><svg  class="icon_btn" onclick="stop_emu_view();" xmlns="http://www.w3.org/2000/svg" width="2.0em" height="2.0em" fill="currentColor" class="bi bi-pause-btn" viewBox="0 0 16 16">
     <path d="M6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5h-3z"/>
     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
 </svg>
 <svg  id="toggle_icon" class="icon_btn" onclick="toggle_run();" xmlns="http://www.w3.org/2000/svg" width="2.0em" height="2.0em" fill="currentColor" class="bi bi-pause-btn" viewBox="0 0 16 16">
+${pause_icon}
 </svg>
 <svg id="btn_unlock_audio" class="icon_btn" onclick="toggle_audio();" xmlns="http://www.w3.org/2000/svg" width="2.0em" height="2.0em" fill="currentColor" class="bi bi-pause-btn" viewBox="0 0 16 16">
+${audio_locked_icon}
 </svg>
 
-<a style="margin-left:auto;border:none;width:1.1em" onclick="stop_emu_view();" href="https://dirkwhoffmann.github.io/virtualc64web/${params}#${address}" target="blank">
-    <svg class="icon_btn" xmlns="http://www.w3.org/2000/svg" width="1.1em" height="1.1em" fill="currentColor" class="bi bi-pause-btn" viewBox="0 0 16 16">
+<a title="open fullwindow in new browser tab" style="margin-left:auto;border:none;width:1.5em;margin-top:4px" onclick="stop_emu_view();" href="https://dirkwhoffmann.github.io/virtualc64web/${params}#${address}" target="blank">
+    <svg class="icon_btn" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-pause-btn" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
-   
 </svg>
 </a>
+</div>
 </div>
 `;
     emu_container.html(emuview_html); 
