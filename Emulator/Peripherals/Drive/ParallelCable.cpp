4f7f816de98286b2c40c01e35d3b9cb782c8e852
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-07-20 15:06:45 +0200

    Regrouping

---
+++ Emulator/Peripherals/Drive/ParallelCable.cpp
@@ -0,0 +1,11 @@
+// -----------------------------------------------------------------------------
+// This file is part of VirtualC64
+//
+// Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
+// Licensed under the GNU General Public License v3
+//
+// See https://www.gnu.org for license information
+// -----------------------------------------------------------------------------
+
+#include "config.h"
+#include "ParallelCable.h"
