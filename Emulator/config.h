a034de579fbbd4c1b0075ecb72e11f302f127b79
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-11-19 13:08:30 +0100

    This is VirtualC64 4.5

--- Emulator/config.h
+++ Emulator/config.h
@@ -17,13 +17,13 @@
 #define VER_MAJOR 4
 #define VER_MINOR 5
 #define VER_SUBMINOR 0
-#define VER_BETA 1
+#define VER_BETA 0
 
 // Snapshot version number
 #define SNP_MAJOR 4
 #define SNP_MINOR 5
 #define SNP_SUBMINOR 0
-#define SNP_BETA 1
+#define SNP_BETA 0
 
 // Uncomment these settings in a release build
 #define RELEASEBUILD
