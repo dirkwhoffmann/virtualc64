c6e96d657540bea5f97a2f4e085f97ff4ab13f8f
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-05-22 12:14:56 +0200

    Restructuring

--- Emulator/VirtualC64Types.h
+++ Emulator/VirtualC64Types.h
@@ -13,40 +13,52 @@
 
 #pragma once
 
-#include "AudioPortTypes.h"
-#include "C64Types.h"
-#include "CartridgeTypes.h"
-#include "CIATypes.h"
-#include "CmdQueueTypes.h"
-#include "ControlPortTypes.h"
-#include "CoreComponentTypes.h"
-#include "CPUTypes.h"
-#include "DatasetteTypes.h"
-#include "DiskTypes.h"
-#include "DiskAnalyzerTypes.h"
-#include "DriveTypes.h"
-#include "EmulatorTypes.h"
-#include "ErrorTypes.h"
-#include "ExpansionPortTypes.h"
-#include "FSTypes.h"
-#include "JoystickTypes.h"
-#include "KeyboardTypes.h"
-#include "MediaFileTypes.h"
-#include "MemoryTypes.h"
-#include "MonitorTypes.h"
-#include "MouseTypes.h"
-#include "MsgQueueTypes.h"
-#include "OptionTypes.h"
-#include "PaddleTypes.h"
-#include "ParallelCableTypes.h"
-#include "PowerPortTypes.h"
-#include "RecorderTypes.h"
-#include "RemoteManagerTypes.h"
-#include "RemoteServerTypes.h"
-#include "RetroShellTypes.h"
-#include "SIDTypes.h"
-#include "ThreadTypes.h"
-#include "UserPortTypes.h"
-#include "VICIITypes.h"
+#include "VC64Core/EmulatorTypes.h"
+
+// Foundation
+#include "VC64Core/Base/CmdQueueTypes.h"
+#include "VC64Core/Base/CoreComponentTypes.h"
+#include "VC64Core/Base/ErrorTypes.h"
+#include "VC64Core/Base/MsgQueueTypes.h"
+#include "VC64Core/Base/OptionTypes.h"
+#include "VC64Core/Base/ThreadTypes.h"
+
+// Components
+#include "VC64Core/Components/C64Types.h"
+#include "VC64Core/Components/CIA/CIATypes.h"
+#include "VC64Core/Components/CPU/CPUTypes.h"
+#include "VC64Core/Components/Memory/MemoryTypes.h"
+#include "VC64Core/Components/SID/SIDTypes.h"
+#include "VC64Core/Components/VICII/VICIITypes.h"
+
+// Ports
+#include "VC64Core/Ports/AudioPortTypes.h"
+#include "VC64Core/Ports/ControlPortTypes.h"
+#include "VC64Core/Ports/ExpansionPortTypes.h"
+#include "VC64Core/Ports/PowerPortTypes.h"
+#include "VC64Core/Ports/UserPortTypes.h"
+
+// Peripherals
+#include "VC64Core/Peripherals/Datasette/DatasetteTypes.h"
+#include "VC64Core/Peripherals/Drive/DriveTypes.h"
+#include "VC64Core/Peripherals/Drive/DiskTypes.h"
+#include "VC64Core/Peripherals/Drive/DiskAnalyzerTypes.h"
+#include "VC64Core/Peripherals/Joystick/JoystickTypes.h"
+#include "VC64Core/Peripherals/Keyboard/KeyboardTypes.h"
+#include "VC64Core/Peripherals/Monitor/MonitorTypes.h"
+#include "VC64Core/Peripherals/Mouse/MouseTypes.h"
+#include "VC64Core/Peripherals/Paddle/PaddleTypes.h"
+#include "VC64Core/Peripherals/Drive/ParallelCableTypes.h"
+
+// Media
+#include "VC64Core/Media/MediaFileTypes.h"
+#include "VC64Core/Media/Cartridges/CartridgeTypes.h"
+#include "VC64Core/FileSystems/FSTypes.h"
+
+// Miscellaneous
+#include "VC64Core/Misc/Recorder/RecorderTypes.h"
+#include "VC64Core/Misc/RemoteServers/RemoteManagerTypes.h"
+#include "VC64Core/Misc/RemoteServers/RemoteServerTypes.h"
+#include "VC64Core/Misc/RetroShell/RetroShellTypes.h"
 
 #include "Constants.h"
